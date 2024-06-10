5d2de254a547be2f6ccade2f68b142b08b842900
FedericoG2 <fg0170700@gmail.com>
2024-06-10 17:28:26 -0300

    Tabla Usuarios en la DB

---
+++ AppCrud/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace AppCrud.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
